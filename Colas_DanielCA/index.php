fe421ada98f5c66ce1f403264f446ef305dc9d85
Daniel Cristancho <dasancristal91@gmail.com>
2025-05-12 08:02:27 -0500

    Add files via upload

---
+++ Colas_DanielCA/index.php
@@ -0,0 +1,17 @@
+<?php
+        $cola =  array();
+        
+        array_push($cola, "Slowdive");
+        array_push($cola, "My Bloody Valentine");
+        array_push($cola, "Pinkshinyultrablast");
+        array_push($cola, "Dottie");
+        array_push($cola, "No Joy");
+        array_push($cola, "Lush");
+        array_push($cola, "A.R. Kane");
+        array_push($cola, "Flyying Colours");
+        array_push($cola, "Ride");
+
+        $elemento = array_shift($cola);
+        echo "el primer elemento que llegó a la cola es: ".$elemento
+        
+?>
